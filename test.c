bac636c9d94b620887ad3715bc07e3fe4eb66c0a
ganyu2233 <1445646263@qq.com>
2021-06-25 17:06:44 +0800

    测试6

---
+++ test.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+#define LINE 30int 
+main(){    int i, j;    for (i = 0; i < LINE; i++)
+    {        for (j = 0; j < (LINE - i); j++)
+    {
+        printf(" ");
+    }        for (j = 0; j < 2 * i + 1; j++)
+    {
+        printf("*");
+    }        printf("\n");
+    }     return 0;
+}
