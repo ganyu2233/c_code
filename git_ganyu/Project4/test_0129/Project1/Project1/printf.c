c9d4cd91528b23dd8bca75f01f8844eecfc5335b
ganyu2233 <86455834+ganyu2233@users.noreply.github.com>
2023-02-08 23:22:25 +0800

    Add files via upload

---
+++ git_ganyu/Project4/test_0129/Project1/Project1/printf.c
@@ -0,0 +1,12 @@
+#define _CRT_SECURE_NO_WARNINGS 1
+#include <stdio.h>
+int main(void)
+{
+	int bph2o = 212;
+	int rv;
+	rv = printf("%d F is water's boiling point.\n ", bph2o);
+	printf("The printf() function printed %d characters.\n", rv);
+	return 0;
+
+}
+//淘宝买东西，牛肉；出差准备
