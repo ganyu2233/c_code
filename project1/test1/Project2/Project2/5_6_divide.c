9de2c9bc6bd6be0b10e8df6709cf056073b51503
ganyu2233 <1445646263@qq.com>
2023-02-20 22:27:33 +0800

    123

---
+++ project1/test1/Project2/Project2/5_6_divide.c
@@ -0,0 +1,13 @@
+#define _CRT_SECURE_NO_WARNINGS 1
+#include <stdio.h>
+#include <stdlib.h>
+int main(void)
+{
+	printf("intger division: 5/4  is  %d \n", 5 / 4);
+	printf("intger division: 6/3  is  %d \n", 6 / 3);
+	printf("intger division: 7/4  is  %d \n", 7 / 4);
+	printf("intger division: 7./4.  is  %1.2f \n", 7. / 4.);
+	printf("intger division: 7./4  is  %1.2f \n", 7. / 4);
+	return 0;
+
+}
